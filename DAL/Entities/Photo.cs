0ccbbabc4d823bae4ee8e23f5176d050a1f8159d
SergioF <fs220381@gmail.com>
2020-11-06 06:54:15 +0200

    User, Role, Photo entities added

---
+++ DAL/Entities/Photo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Photo
+    {
+        public int PhotoId { get; set; }
+        public string PhotoTitle { get; set; }
+        public DateTime DateTimeUploading { get; set; }
+        public Raiting Raiting { get; set; }
+    }
+
+    public class Raiting
+    {
+        public int VoicesCount { get; set; }
+        public float CurrentRaiting { get; set; }
+    }
+}
