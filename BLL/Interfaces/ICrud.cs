289eeaad4d2a4677b5d3cde2f2a75c4ada55bada
SergioF <fs220381@gmail.com>
2020-11-11 05:36:37 +0200

    Automapper profile added

---
+++ BLL/Interfaces/ICrud.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface ICrud<TModel> where TModel : class
+    {
+        IEnumerable<TModel> GetAll();
+        Task<TModel> GetByIdAsync(int id);
+        Task AddAsync(TModel model);
+        Task UpdateAsync(TModel model);
+        Task DeleteByIdAsync(int id);
+
+    }
+}
